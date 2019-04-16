53c5f384ffb141116fdde1100155f225ed24681b
Kori <icedragon668@gmail.com>
2019-04-15 23:46:28 -0400

    missed Body.js commit

--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -26,16 +26,11 @@ class Body extends React.Component {
         speil : "I am a lifelong learner, motivated to learn new technology, languages, and even workplace paradigms. Strengths include JS, Express, Node, React, and SVG with an inclination towards animation. I have a passion for game design and development. With previous experience in team management and as an EMT, I have developed an ability to work in fast-paced environments while effectively managing priorities.",
     }
 
-    componentWillMount() {
-        console.log( this.props.onLoad(this.state.title))
-        // this.setState({title : this.props.onLoad(this.state.title)}, {speil : this.props.onLoad(this.state.speil)})
-    }
     componentDidMount = () =>{
         const newTitle = this.props.onLoad(this.state.title)
         document.getElementById('title').innerHTML = newTitle
     }
     render() {
-        // this.translate()
         return (
         <div className="container">
             <p id="title"></p>
